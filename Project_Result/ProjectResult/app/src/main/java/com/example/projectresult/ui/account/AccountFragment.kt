c62ab84ddc8f50d19f4e37d7a36f60b922449804
SSongy <gypig0927@naver.com>
2019-11-17 21:25:41 +0900

    기본적인 앱 UI

---
+++ Project_Result/ProjectResult/app/src/main/java/com/example/projectresult/ui/account/AccountFragment.kt
@@ -0,0 +1,27 @@
+package com.example.projectresult.ui.account
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.projectresult.R
+
+class AccountFragment : Fragment() {
+
+    private lateinit var accountViewModel: AccountViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        accountViewModel =
+            ViewModelProviders.of(this).get(AccountViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_account, container, false)
+        return root
+    }
+}
---
+++ Project_Result/ProjectResult/app/src/main/java/com/example/projectresult/ui/home/NutrientFragment.kt
@@ -0,0 +1,19 @@
+package com.example.projectresult.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.projectresult.R
+
+class NutrientFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_subhome_nutrient, container, false)
+    }
+}
