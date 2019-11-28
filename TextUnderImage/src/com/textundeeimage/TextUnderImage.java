b8a23affef36ef0634313110c66793a118f502fd
ucando222 <51014882+ucando222@users.noreply.github.com>
2019-11-28 23:12:05 +0900

    Add files via upload

---
+++ TextUnderImage/src/com/textundeeimage/TextUnderImage.java
@@ -0,0 +1,13 @@
+package com.textundeeimage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TextUnderImage extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
