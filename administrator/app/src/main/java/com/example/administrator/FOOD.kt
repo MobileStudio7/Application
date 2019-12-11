b516c1750e44b3a45069bd6e6c81cc4386459a22
lsw8309 <smy02244@naver.com>
2019-12-11 10:26:21 +0900

    관리자모드

---
+++ administrator/app/src/main/java/com/example/administrator/FOOD.kt
@@ -0,0 +1,3 @@
+package com.example.administrator
+
+class FOOD(val id: Int, val name: String, val serving_sie: Int, val calrorie: Double, val carbohydrate: Double, val protein: Double, val fat: Double)
